aa8b7fea9f032874806acba991d0bac72e34f862
victor-carvalho1996 <victorpcarvalho18@gmail.com>
2022-04-13 19:34:01 -0300

    02

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-function CheckPalindromo() {
-	let palavra = $("#palavra").val();
-	palavra = palavra.toLowerCase();
-	let palavraInverse = ""; 
+function CheckPalindromo(p_palavra) {
+	let palavra = p_palavra.replace(/\s/g, "").toLowerCase();
 
-	for (var i = palavra.length - 1; i >= 0; i--) {
-		if (palavra[i] !== " ") {
-			palavraInverse += palavra[i];
+	let countPalavra = Math.ceil(palavra.length / 2);
+	let countAux = 0; 
+	let isPalindromo = true;
+	while (countPalavra !== countAux) {
+		if(palavra[countAux] !== palavra[(palavra.length - 1) - countAux]) {
+			isPalindromo = false;
+			break;
 		}
+		countAux++;
 	}
-
-	if (palavra.replace(/\s/g, "") === palavraInverse) {
-		alert("A palavra é um palindromo.");
+	if (isPalindromo) {
+		console.log("A palavra é um palindromo.");
 	}else {
-		alert("A palavra não é um palindromo.");
+		console.log("A palavra não é um palindromo.");
 	}
 
 }
